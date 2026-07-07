b081bf8110b0410230ca50111c255d7c3a7b5756
LAB4804WD09 <jeffreysilvas09@gmail.com>
2026-07-07 11:01:14 -0700

    Created functionality where dice is rolled 100 times, and the frequency of each result is tracked and printed.

--- Main.java
+++ Main.java
@@ -1,11 +1,33 @@
 import java.util.Random;
 
-class Main
+public class Main
 {
   public static void main(String[] args)
   {
-    // Example calling the roll() method
-    System.out.println("Roll: " + roll());
+    int count1, count2, count3, count4, count5, count6;
+    count1 = count2 = count3 = count4 = count5 = count6 = 0;
+
+    for(int i = 0; i < 100; i++) {
+      int dice = roll();
+      switch (dice) {
+        case 1 -> count1++;
+        case 2 -> count2++;
+        case 3 -> count3++;
+        case 4 -> count4++;
+        case 5 -> count5++;
+        case 6 -> count6++;
+      }
+      System.out.printf("Roll: [%d]%n", dice);
+    }
+
+    System.out.printf("Final roll counts:%n" +
+            "[1]: %d times%n" +
+            "[2]: %d times%n" +
+            "[3]: %d times%n" +
+            "[4]: %d times%n" +
+            "[5]: %d times%n" +
+            "[6]: %d times%n",
+            count1, count2, count3, count4, count5, count6);
   }
   
   // Random is a class we can use to generate random numbers
